829bc4e11105b34a11c6a9dfc5192331efb6edd0
Hasan K. Tosun <hktosun@gmail.com>
2021-10-08 17:05:14 -0500

    update data

---
+++ R/data.R
@@ -0,0 +1,31 @@
+#' List of school districts in Minnesota
+#'
+#' A dataset containing the list of school districts (names and id's).
+#'
+#'
+#' @format A data frame
+"school_districts"
+
+#' List of counties in Minnesota
+#'
+#' A dataset containing the list of counties (names and id's).
+#'
+#'
+#' @format A data frame
+"counties"
+
+#' List of disabilities available in the website
+#'
+#' A dataset containing the list of disabilities available in the website
+#'
+#'
+#' @format A data frame
+"disabilities"
+
+#' List of languages available in the website
+#'
+#' A dataset containing the list of languages available in the website
+#'
+#'
+#' @format A data frame
+"languages"
--- R/save_data.R
+++ R/save_data.R
@@ -1,4 +1,8 @@
-# school_districts <- read_csv("aux_files/school_districts.csv") %>%
-# 	mutate(sd_id = paste0(school_district_id, school_district_type))
+# school_districts <- readr::read_csv("data_raw/school_districts.csv") %>%
+# 	dplyr::mutate(sd_id = paste0(school_district_id, school_district_type))
 #
-# usethis::use_data(school_districts, internal = TRUE, overwrite = TRUE)
+# counties <- readr::read_csv("data_raw/counties.csv")
+# disabilities <- readr::read_csv("data_raw/disabilities.csv")
+# languages <- readr::read_csv("data_raw/languages.csv")
+#
+# usethis::use_data(school_districts, counties, disabilities, languages, internal = TRUE, overwrite = TRUE)
