3e27e0254d624541a55a028e9f6b9a019cb105a4
Hasan K. Tosun <hktosun@gmail.com>
2021-10-08 21:13:49 -0500

    fix sd data

--- R/save_data.R
+++ R/save_data.R
@@ -1,5 +1,7 @@
 # school_districts <- readr::read_csv("data_raw/school_districts.csv") %>%
-# 	dplyr::mutate(sd_id = paste0(school_district_id, school_district_type))
+# 	dplyr::mutate(sd_id = paste0(school_district_id, school_district_type)) %>%
+# 	dplyr::mutate(school_district_id = paste0(school_district_id, "-", school_district_type)) %>%
+# 	dplyr::select(sd_id, school_district_id, school_district_name)
 #
 # counties <- readr::read_csv("data_raw/counties.csv")
 # disabilities <- readr::read_csv("data_raw/disabilities.csv")
